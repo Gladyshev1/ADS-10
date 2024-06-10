867f4ab69c97ef0198e4a87dc31695f31617c0ab
Гладышев Максим <maksimgladysev731@gmail.com>
2024-06-10 22:05:57 +0300

    a10

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,40 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <iostream>
+#include <vector>
+
+struct Node {
+    std::vector<Node*> vecch;
+    char data;
+};
+
+class MYtree {
+ public:
+
+     void add(std::vector<char>, Node*);
+     explicit MYtree(std::vector<char>);
+    Node* root;
+
+ private:
+    int size;
+
+
+};
+
+inline void MYtree::add(std::vector<char> nach, Node* root) {
+    for (int i = 0; i < nach.size(); i++) {
+        Node* node = new Node;
+        node->data = nach[i];
+        root->vecch.push_back(node);
+        std::vector<char> vecEnd = nach;
+        vecEnd.erase(vecEnd.begin() + i);
+        add(vecEnd, node);
+    }
+}
+inline MYtree::MYtree(std::vector<char> nach) {
+    root = new Node;
+    add(nach, root);
+}
+
 #endif  // INCLUDE_TREE_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,37 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+
+int factorial(int i) {
+    int ret = 1;
+    for (int Q = 1; Q <= i; Q++) {
+        ret *= Q;
+    }
+    return ret;
+}
+
+std::vector<char> getPerm(const MYtree& tree, int X) {
+    std::vector<char> vec;
+    if (X <= factorial(tree.root->vecch.size())) {
+        auto root = tree.root;
+        int i = factorial(root->vecch.size()) / root->vecch.size();
+        for (int Q = 0; Q < root->vecch.size(); Q++) {
+            if (X > Q * 2 && X <= i * (Q + 1)) {
+                root = root->vecch[Q];
+                vec.push_back(root->data);
+            }
+        }
+        if (X % 2 != 1) {
+            root = root->vecch[1];
+            vec.push_back(root->data);
+           
+        }
+        else {
+            root = root->vecch[0];
+            vec.push_back(root->data);
+        }
+        root = root->vecch[0];
+        vec.push_back(root->data);
+    }
+    return vec;
 }
